c048543fad0997bf3cbc86a3785fe59947c0d26c
feststelltaste <feststelltaste@googlemail.com>
2012-03-04 12:03:46 +0100

    change test to french cards

--- notrick/src/test/java/de/unitude/notrick/cards/card/CardTest.java
+++ notrick/src/test/java/de/unitude/notrick/cards/card/CardTest.java
@@ -4,16 +4,16 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-import de.unitude.notrick.cards.card.Card;
-import de.unitude.notrick.cards.card.color.GermanColor;
-import de.unitude.notrick.cards.card.type.GermanType;
+import de.unitude.notrick.cards.card.type.FrenchType;
+import de.unitude.notrick.cards.deck.FrenchColor;
 
 public class CardTest {
 
 	@Test
 	public void testGetName() {
-		Card c = new Card(GermanColor.HERZ, GermanType.KOENIG);
-		assertEquals("Herzkönig", c.getName());
+		Card c = new Card(FrenchColor.CLUB, FrenchType.ACE);
+		assertEquals(0, c.getColor().getPriority());
+		assertEquals(0, c.getType().getPriority());
 	}
 
 }
