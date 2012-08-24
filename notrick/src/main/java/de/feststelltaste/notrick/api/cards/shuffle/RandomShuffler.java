30c836c339c97ee6181c606e1ec1171afbea3370
feststelltaste <feststelltaste@googlemail.com>
2012-08-24 22:38:11 +0200

    test random shuffler (a bit)

--- notrick/src/main/java/de/feststelltaste/notrick/api/cards/shuffle/RandomShuffler.java
+++ notrick/src/main/java/de/feststelltaste/notrick/api/cards/shuffle/RandomShuffler.java
@@ -1,30 +1,27 @@
-/**
- * update test bla
- * test2
- */
-/**
- * update test run
- */
-/**
- * test
- */
 package de.feststelltaste.notrick.api.cards.shuffle;
 
 import java.util.Collections;
+import java.util.Random;
 
 import de.feststelltaste.notrick.api.cards.deck.CardDeck;
 
 public class RandomShuffler implements Shuffler {
 
     private CardDeck cardDeck;
+    private Random random;
+
+    public RandomShuffler(CardDeck cardDeck, Random random) {
+	this.cardDeck = cardDeck;
+	this.random = random;
+    }
 
     public RandomShuffler(CardDeck cardDeck) {
-	this.cardDeck = cardDeck;
+	this(cardDeck, new Random());
     }
 
     @Override
     public CardDeck shuffleCards() {
-	Collections.shuffle(cardDeck.getAsList());
+	Collections.shuffle(cardDeck.getAsList(), random);
 	return cardDeck;
     }
 
---
+++ notrick/src/test/java/de/feststelltaste/notrick/api/cards/shuffle/RandomShufflerTest.java
@@ -0,0 +1,39 @@
+package de.feststelltaste.notrick.api.cards.shuffle;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+import java.util.Random;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.feststelltaste.notrick.api.cards.card.Card;
+import de.feststelltaste.notrick.api.cards.card.suit.FrenchSuit;
+import de.feststelltaste.notrick.api.cards.card.type.FrenchType;
+import de.feststelltaste.notrick.api.cards.deck.CardDeck;
+import de.feststelltaste.notrick.api.cards.deck.FrenchShortCardDeck;
+
+public class RandomShufflerTest {
+
+    RandomShuffler r;
+
+    @Before
+    public void setUp() throws Exception {
+	r = new RandomShuffler(new FrenchShortCardDeck(), new Random(1l));
+    }
+
+    @Test
+    public void testShuffleCards() {
+	CardDeck c = r.shuffleCards();
+	List<Card> cards = c.getAsList();
+	Card firstTestCard = new Card(FrenchSuit.HEART, FrenchType.ACE);
+	Card middleTestCard = new Card(FrenchSuit.HEART, FrenchType.QUEEN);
+	Card lastTestCard = new Card(FrenchSuit.HEART, FrenchType.SEVEN);
+	assertEquals(firstTestCard, cards.get(0));
+	assertEquals(middleTestCard, cards.get(cards.size() / 2));
+	assertEquals(lastTestCard, cards.get(cards.size() - 1));
+
+    }
+
+}
