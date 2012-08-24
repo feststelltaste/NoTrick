cf9decfcb376bcd553503d4e088ae747bb178a35
feststelltaste <feststelltaste@googlemail.com>
2012-08-24 22:35:20 +0200

    add equals method

--- notrick/src/main/java/de/feststelltaste/notrick/api/cards/card/Card.java
+++ notrick/src/main/java/de/feststelltaste/notrick/api/cards/card/Card.java
@@ -17,6 +17,36 @@ public class Card {
 	return suit;
     }
 
+    @Override
+    public int hashCode() {
+	int hash = 31;
+	hash = hash * 17 + (this.type != null ? type.getPriority() : 0);
+	hash = hash * 23 + (this.type != null ? suit.getPriority() : 0);
+	return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+	if (this == obj)
+	    return true;
+	if (obj == null)
+	    return false;
+	if (getClass() != obj.getClass())
+	    return false;
+	Card other = (Card) obj;
+	if (suit == null) {
+	    if (other.suit != null)
+		return false;
+	} else if (!suit.equals(other.suit))
+	    return false;
+	if (type == null) {
+	    if (other.type != null)
+		return false;
+	} else if (!type.equals(other.type))
+	    return false;
+	return true;
+    }
+
     public String getName() {
 	return suit.getName() + type.getName().toLowerCase();
     }
--- notrick/src/test/java/de/feststelltaste/notrick/api/cards/card/CardTest.java
+++ notrick/src/test/java/de/feststelltaste/notrick/api/cards/card/CardTest.java
@@ -1,20 +1,57 @@
 package de.feststelltaste.notrick.api.cards.card;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import de.feststelltaste.notrick.api.cards.card.Card;
 import de.feststelltaste.notrick.api.cards.card.suit.FrenchSuit;
 import de.feststelltaste.notrick.api.cards.card.type.FrenchType;
 
 public class CardTest {
 
+    Card card = new Card(FrenchSuit.CLUB, FrenchType.ACE);
+
     @Test
-    public void testGetName() {
-	Card c = new Card(FrenchSuit.CLUB, FrenchType.ACE);
-	assertEquals(0, c.getSuit().getPriority());
-	assertEquals(0, c.getType().getPriority());
+    public void NullIsNotEqual() {
+	assertFalse(card.equals(null));
+    }
+
+    @Test
+    public void FalseTypeIsNotEqual() {
+	assertFalse(card.equals(new Object()));
+    }
+
+    @Test
+    public void UnequalCardIsNotEqual() {
+	assertFalse(card.equals(new Card(FrenchSuit.CLUB, FrenchType.EIGTH)));
+    }
+
+    @Test
+    public void EqualCardIsEqual() {
+	assertTrue(card.equals(new Card(FrenchSuit.CLUB, FrenchType.ACE)));
+    }
+
+    @Test
+    public void SameCardIsEqual() {
+	assertTrue(card.equals(card));
+    }
+
+    @Test
+    public void testHashCode() {
+	assertEquals(12123, new Card(FrenchSuit.HEART, FrenchType.ACE).hashCode());
+	assertEquals(12121, new Card(FrenchSuit.CLUB, FrenchType.ACE).hashCode());
+    }
+
+    @Test
+    public void checkGetNameFormat()
+    {
+	assertEquals("clubace", card.getName());
+    }
+    
+    @Test
+    public void checkToStringFormat()
+    {
+	assertEquals("0:0: clubace", card.toString());
     }
 
 }
