7315be4f3ef0ba8e533bca5b3e51d58f2cd9066d
feststelltaste <feststelltaste@googlemail.com>
2012-08-25 11:05:24 +0200

    add human and computer players

---
+++ notrick/src/test/java/de/feststelltaste/notrick/api/player/HumanPlayerTest.java
@@ -0,0 +1,22 @@
+package de.feststelltaste.notrick.api.player;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.feststelltaste.notrick.api.cards.card.GermanCardFactory;
+
+public class HumanPlayerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+    
+    @Test
+    public void behaviour(){
+	HumanPlayer p = new HumanPlayer();
+	p.take(GermanCardFactory.create("S", "7"));
+	
+    }
+
+}
